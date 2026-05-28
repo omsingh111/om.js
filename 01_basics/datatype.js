6ee354ca2cc252f908ec4b6e6fb062b08f1781e7
omsingh111 <omsingh20052005@gmail.com>
2026-05-28 05:00:36 +0000

    save

---
+++ 01_basics/datatype.js
@@ -0,0 +1,13 @@
+//let name = 33
+let type_name = 33
+let num = String(type_name)
+
+console.log(type_name);
+console.log(typeof type_name);
+console.log(num);
+console.log(typeof num);
+//remember to put s as capital String
+
+
+
+
--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,1 +1,7 @@
 console.log("variables")
+const account_id =12344
+var account_code="om12345"
+let account_mail ="om@ds.in"
+
+console.log(account_id)
+console.log(3+3)
