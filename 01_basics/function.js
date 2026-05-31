2cf3c7ebaf11d88e9458d8f0383ef4664b7e3e4c
omsingh111 <omsingh20052005@gmail.com>
2026-05-31 06:11:28 +0000

    function

---
+++ 01_basics/function.js
@@ -0,0 +1,16 @@
+// there are two ways to define a function
+//1.conventional or named function 
+
+function add(a,b){
+return a+b
+}
+
+console.log(add(2,4))
+
+//2. unnnamed function
+const sum=function(a,b){
+
+    return a+b
+}
+
+console.log(sum(4,8))
